e214486e8ae4a69ac6649cdd84f3033109a09222
travrens <sarahrenhome@gmail.com>
2023-02-14 20:09:49 -0800

    itworks

--- air_cownditioning_II.py
+++ air_cownditioning_II.py
@@ -1,22 +1,65 @@
+# n, m = (int(x) for x in input().split())
+# regions = []
+# for _ in range(n):
+#     regions.append([int(x) for x in input().split()])
+# acs = []
+# for _ in range(m):
+#     acs.append([int(x) for x in input().split()])
+# ret = sum([x[3] for x in acs])
+# for used in range(2**m):
+#     cool = [0] * 101
+#     cost = 0
+#     for i in range(m):
+#         if used & (2 ** i):
+#             cost += acs[i][3]
+#             for x in range(acs[i][0], acs[i][1] + 1):
+#                 cool[x] += acs[i][2]
+#     valid = True
+#     for i in range(n):
+#         valid = valid and all([cool[x] >= regions[i][2] for x in range(regions[i][0], regions[i][1] + 1)])
+#     if valid:
+#         ret = min(ret, cost)
+# print(ret)
+
+
 n, m = (int(x) for x in input().split())
-regions = []
-for _ in range(n):
-    regions.append([int(x) for x in input().split()])
+reqs = []
+for i in range(n):
+    reqs.append([int(x) for x in input().split()])
 acs = []
-for _ in range(m):
+for i in range(m):
     acs.append([int(x) for x in input().split()])
-ret = sum([x[3] for x in acs])
-for used in range(2**m):
-    cool = [0] * 101
+sol = sum([ac[3] for ac in acs])
+
+def ItWorks(matched):
+    works = True
+    cooling = [0] * 10
     cost = 0
-    for i in range(m):
-        if used & (2 ** i):
-            cost += acs[i][3]
-            for x in range(acs[i][0], acs[i][1] + 1):
-                cool[x] += acs[i][2]
-    valid = True
-    for i in range(n):
-        valid = valid and all([cool[x] >= regions[i][2] for x in range(regions[i][0], regions[i][1] + 1)])
-    if valid:
-        ret = min(ret, cost)
-print(ret)
+    for x in matched:
+        cost += acs[x][3]
+        for w in range(acs[x][0], acs[x][1]+1):
+            cooling[w] += acs[x][2]
+    for cow in reqs:
+        if not works:
+            break
+        for q in range(cow[0], cow[1]+1):
+            if cooling[q] < cow[2]:
+                works = False
+                break
+    if works:
+        return [True, cost]
+    return [False, 0]
+
+
+for i in range(2**m):
+    matched = []
+    for j in range(m):
+        if i & 2**j:
+            matched.append(j)
+    if not matched:
+        continue
+    # print(matched)
+    works, cost = ItWorks(matched)
+    if works:
+        sol = min(cost, sol)
+print(sol)
